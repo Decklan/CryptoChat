e5765bb00730b45fb6f9074b12d66f385abcd57d
Decklan <decklan007@gmail.com>
2018-10-20 14:32:23 -0700

    Modified UserController to use class
    Created the UserController as a class and set callback functions for
    each of the api routes that the client can request information from.
    Export the router so that it can be used in app.ts for route handling.

--- chat-server/src/controllers/user.controller.ts
+++ chat-server/src/controllers/user.controller.ts
@@ -1,30 +1,72 @@
 import { Router, Request, Response } from 'express';
 
-const router: Router = Router();
+/**
+ * UsersController for handling routing requests to '/users'
+ * Exports the class' router as UserController to be used in 
+ * app.ts
+ */
+class UsersController {
+    public router: Router;
 
-// Route for retrieving all users
-router.get('/', (req: Request, res: Response) => {
-    // Simple console log for now
-    console.log(req.body);
-});
+    constructor() {
+        this.router = Router();
+        this.routes();
+    }
 
-// Route for retrieving a single user by their unique id
-router.get('/:id', (req: Request, res: Response) => {
-    // Simple console log for now
-    console.log(req.body);
-});
+    /**
+     * Route for getting all the currently active users in the application
+     * @param req Request from the client
+     * @param res Response from the server
+     */
+    public GetActiveUsers(req: Request, res: Response) {
+        console.log(req.body);
+    }
 
-// Route for adding a new user to the database
-router.post('/', (req: Request, res: Response) => {
-    // Simple console log for now
-    console.log(req.body);
-});
+    /**
+     * Route for getting a user by their unique id
+     * @param req Request from the client
+     * @param res Response from the server
+     */
+    public GetUserById(req: Request, res: Response) {
+        console.log(req.params.id);
+    }
 
-// Route for updating a user with a matching unique id
-router.put('/:id', (req: Request, res: Response) => {
-    // Simple console log for now
-    console.log(req.body);
-});
+    /**
+     * Route for creating a new user 
+     * @param req Request from the client
+     * @param res Response from the server
+     */
+    public CreateNewUser(req: Request, res: Response) {
+        console.log(req.body);
+    }
 
-// Exports the router instance to be used in app.ts
-export const UserController: Router = router;
+    /**
+     * Route for updating a user by their unique id
+     * @param req Request from the client
+     * @param res Response from the server
+     */
+    public UpdateUserById(req: Request, res: Response) {
+        console.log(req.params.id);
+    }
+
+    /**
+     * Takes each route and sets the callback function to be called
+     * when that route is requested
+     */
+    public routes(): void {
+        this.router.get('/', this.GetActiveUsers);
+        this.router.get('/:id', this.GetUserById);
+        this.router.post('/', this.CreateNewUser);
+        this.router.put('/:id', this.UpdateUserById);
+    }
+}
+
+/**
+ * 1. Instantiate the UsersController class
+ * 2. Invoke the usersController routes() method to set callbacks
+ * 3. Export the usersController's router to be used in app.ts
+ *    (named UserController by convention) 
+ */
+const usersController: UsersController = new UsersController();
+usersController.routes();
+export const UserController: Router = usersController.router;
