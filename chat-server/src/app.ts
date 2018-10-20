75a794078bc0ae7846f0e7c7987e5c5635a86c70
Decklan <decklan007@gmail.com>
2018-10-20 13:43:48 -0700

    Added cors policy
    Added cors middleware for making CORS request from the Angular client.

--- chat-server/src/app.ts
+++ chat-server/src/app.ts
@@ -1,10 +1,16 @@
 import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 import { createConnection } from 'typeorm';
 
 // Import controllers
 import { UserController } from './controllers';
 
-// Setup application
+/**
+ * Set up the express application
+ *  - For some reason using the typescript import style 
+ *    for this doesn't work so I'm sticking with the require
+ *    style for now
+ */
 const express = require('express');
 const app = express();
 
@@ -12,8 +18,16 @@ const app = express();
  * Setup the various middlewares used in the application
  *  * bodyParser creates a body in the request containing
  *    any json data sent to the server in the request
+ *  * cors middleware allows cross origin resource sharing
+ *    so that, during development, you don't run into 500
+ *    server errors when requesting resources from a different
+ *    origin than the server (eg. localhost:4200 for Angular)
  */
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
 
 /**
  * Mount each of the controllers to a specified route
